e110fa5b0ca204451af0524b1611edac29768478
pkwietniewski <kniarzyn@gmail.com>
2018-06-05 13:55:02 +0200

    Dodaje React i React.DOM + pierwszy komponent

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("My minimal boilerplate")
+import React from "react"
+import ReactDOM from "react-dom"
+
+const App = () => {
+  return (
+    <h1>My minimal react WebPabck Babel Setup!</h1>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
