bd32ae2a36fbf9f5449d6ab1cf2c2c191eddf6f0
pkwietniewski <kniarzyn@gmail.com>
2018-06-05 13:53:28 +0200

    Dodaje zmienną środowiskową NODE_ENV
    Narazie jest prowizorka, poczytać o środowisku produkcyjnym i jak
    uzależnić konfigurację od środowiska

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,22 @@
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = "development"
+}
 module.exports = {
   entry: [
     './src/index.js'
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
